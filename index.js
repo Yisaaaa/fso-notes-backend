0a6c2291a7f89be6502e408e158a7bb54711d06f
Yisaaaa <ryansanisit19@gmail.com>
2023-12-26 19:53:58 +0800

    Installed dotenv and added an environment variables for port and password

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const app = express();
-
-const passwd = process.argv[2];
-const url = `mongodb+srv://ryansanisit19:${passwd}@fso-notes.s097f27.mongodb.net/Person?retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-	content: String,
-	important: Boolean,
-});
-
-const Note = mongoose.model("Note", noteSchema);
+const Note = require("./models/note");
 
 const requestLogger = (request, response, next) => {
-	console.log("Method:", request.method);
-	console.log("Path:  ", request.path);
-	console.log("Body:  ", request.body);
-	console.log("---");
-	next();
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
 };
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" });
+    response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(cors());
@@ -52,62 +40,62 @@ app.use(express.static("dist"));
 // ];
 
 app.get("/", (req, res) => {
-	res.send("<h1>Hello World!</h1>");
+    res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (req, res) => {
-	Note.find({}).then((notes) => res.json(notes));
+    Note.find({}).then((notes) => res.json(notes));
 });
 
 const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
+    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+    return maxId + 1;
 };
 
 app.post("/api/notes", (request, response) => {
-	const body = request.body;
+    const body = request.body;
 
-	if (!body.content) {
-		return response.status(400).json({
-			error: "content missing",
-		});
-	}
+    if (!body.content) {
+        return response.status(400).json({
+            error: "content missing",
+        });
+    }
 
-	const note = {
-		content: body.content,
-		important: body.important || false,
-		date: new Date(),
-		id: generateId(),
-	};
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    };
 
-	notes = notes.concat(note);
+    notes = notes.concat(note);
 
-	response.json(note);
+    response.json(note);
 });
 
 app.get("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
+    const id = Number(request.params.id);
+    const note = notes.find((note) => note.id === id);
 
-	if (note) {
-		response.json(note);
-	} else {
-		response.status(404).end();
-	}
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
 
-	response.json(note);
+    response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	notes = notes.filter((note) => note.id !== id);
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
 
-	response.status(204).end();
+    response.status(204).end();
 });
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,27 +1,23 @@
 const mongoose = require("mongoose");
 
 if (process.argv.process < 3) {
-	console.log("give password as an argument");
-	process.exit(1);
+    console.log("give password as an argument");
+    process.exit(1);
 }
-
-const passwd = process.argv[2];
-
-const url = `mongodb+srv://ryansanisit19:${passwd}@fso-notes.s097f27.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
-	content: String,
-	important: Boolean,
+    content: String,
+    important: Boolean,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-	content: "The queen",
-	important: false,
+    content: "The queen",
+    important: false,
 });
 
 // note.save().then((result) => {
@@ -31,6 +27,6 @@ const note = new Note({
 // });
 
 Note.find({}).then((result) => {
-	result.forEach((note) => console.log(note));
-	mongoose.connection.close();
+    result.forEach((note) => console.log(note));
+    mongoose.connection.close();
 });
